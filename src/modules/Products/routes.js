6f58d80073ad503323a18085902b2c13d2d06d7e
dcvela23 <dcvela@outlook.es>
2022-01-21 00:57:13 +0100

    feat: handle not found route

--- src/modules/Products/routes.js
+++ src/modules/Products/routes.js
@@ -1,6 +1,6 @@
 // react
 import React from "react";
-import { Routes ,Route } from 'react-router-dom';
+import {Routes, Route, Navigate } from "react-router-dom";
 
 // views
 import ProductsList from "./views/ProductsList";
@@ -9,6 +9,7 @@ import ProductDetail from "./views/ProductDetail";
 export const moduleRoutes = () => {
   return (
     <Routes>
+      <Route path="*" element={<Navigate replace to="/" />} />
       <Route path="/" element={<ProductsList />} />
       <Route path="/product/:productId" element={<ProductDetail />} />
     </Routes>
